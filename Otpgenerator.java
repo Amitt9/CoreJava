d416087a67d798de1157b798aff915d9af42f5ac
Amitt9 <130979016+Amitt9@users.noreply.github.com>
2023-05-26 23:35:48 +0530

    otp generator

---
+++ Otpgenerator.java
@@ -0,0 +1,49 @@
+package paswword_generator;
+
+import java.util.Random;
+
+public class Otpgenerator {
+
+	@SuppressWarnings({ "static-access", "unused" })
+	public static void main(String[] args) {
+		String num="1234567890";
+		Random ot=new Random();
+		String req= "2134";
+		//char m=num.charAt(ot.nextInt(num.length()));
+		char[] otp=new char[4];
+		int n=1;
+		String string="null";
+		while(!(string.equals(req)))
+		{
+		
+		for (int i=0;i<otp.length;i++)
+		{
+			otp[i]=num.charAt(ot.nextInt(num.length()));
+		}//for loop end
+		String str=new String();
+		 string=str.valueOf(otp);
+		System.out.println(n++ +"    "+string);
+	
+		
+		}//while loop end
+		System.out.println(("req otp"+string));
+		
+	}
+
+//	@Override
+//	public boolean equals(Object obj) {
+//		// TODO Auto-generated method stub
+//		if (obj instanceof Otpgenerator)
+//		return 
+//	}
+	
+
+	@Override
+	public String toString() {
+		return "otp";
+	}
+	
+	
+	
+
+}
